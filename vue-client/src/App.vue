1756be53bc50be25aadc26f8e48ae7d2e2630f01
rowaxl <rowaxl0@gmail.com>
2019-01-23 22:03:53 +0900

    replace vue client

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,14 +1,18 @@
-/* eslint-disable */
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="ui container">
+    <HelloWorld title="WELCOME TO ABSTURSE CHAT"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-    name: 'App',
-};
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
@@ -18,6 +22,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: auto;
+  height: 80vh;
+  position: absolute;
+  top: 5%;
 }
 </style>
---
+++ vue-client/src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <h2>{{ title }}</h2>
+    <div>
+      <ul>
+        <li v-for="message in messages">{{ message }}</li>
+      </ul>
+    </div>
+    <div class="ui segment input_bar">
+        <form class="ui form"
+          @submit.prevent="addMessage">
+            <div class="field">
+                <input type="text" v-model="newMessage"/>
+            </div>
+        </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String
+  },
+  data: {
+    messages: ['message1', 'message2'],
+    newMessage: ''
+  },
+  methods: {
+    addMessage: (e) => {
+      console.log('this ', this);
+      this.message.push(this.newMessage);
+      this.newMessage = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.input_bar {
+  position: absolute;
+  bottom: 5%;
+  width: 80vw;
+}
+</style>
